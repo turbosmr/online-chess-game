fc51303b59397a0b18558002b60e57c81b35b66c
Balbalnom <diandianhanxing@gmail.com>
2019-04-06 21:47:05 -0700

    8081

--- server.js
+++ server.js
@@ -1,25 +1,97 @@
 var express = require('express');
+var http = require('http');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var bodyParser = require('body-parser')
-//Keep adding Rounter under this
-var server = express();
-var PORT = 5000;
-server.listen(PORT, () => console.log('Server started on port '+PORT+''));
-server.use(logger('dev'));
-server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({ extended: false }));
+var socketIO = require('socket.io');
+var mysql = require('mysql')
 
-server.use(logger('dev'));
-server.use(express.json());
-server.use(express.urlencoded({ extended: false }));
-server.use(cookieParser());
-server.use(express.static(path.join(__dirname, 'public')));
+var app = express();
+var server = http.Server(app);
+var io = socketIO(server);
 
-server.use('/', indexRouter);
-server.use('/users', usersRouter);
+app.set('port', 8081);
+app.use(express.static(path.join(__dirname, 'public')));
 
-module.exports = server;
+/* Routing */
+// Display index.html
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/public/index.html');
+});``
+
+// Display login.html
+app.get('/login', function(req, res){
+    res.sendFile(__dirname + '/public/login.html');
+});
+// Display login.html
+app.get('/lobby', function(req, res){
+    res.sendFile(__dirname + '/public/lobby.html');
+});
+
+/* Create database connection */
+var pool = mysql.createPool({
+    connectionLimit : 100,
+    host            : 'localhost',
+    user            : 'root',
+    password        : 'password',
+    database        : 'chatroom'
+})
+
+/* Check for server to database connection */
+pool.getConnection((err) => {
+    if(err){
+        console.log('Error connecting to DB.');
+    }
+    else{
+        console.log('Database connection established.');
+    }
+});
+
+var socketCount = 0;
+
+/* Check for connection between client and server */
+io.on('connection', (socket) => {
+    // Track which user connects/disconnect and how many users connected
+    socketCount++;
+    console.log('User connected');
+    console.log('Connected users: ' + socketCount);
+    socket.emit('users connected', socketCount);
+    socket.on('disconnect', function(){
+        socketCount--;
+        console.log('User disconnected');
+        console.log('Connected users: ' + socketCount);
+        socket.emit('users connected', socketCount);
+    });
+
+    // Retrieve messages from DB
+    pool.query('SELECT * FROM lobbyChat', (err, result) => {
+        if(err){
+            console.log('Error selecting messages from DB.');
+        }
+        else{
+            // Iterate over messages obtained from DB and send to client
+            for (let index = 0; index < result.length; index++) {
+                socket.emit('retrieve messages', result[index]);
+            }
+        }
+    });
+
+    // Store and display messages to connected clients, as well as console
+    socket.on('chat message', (msg) => {
+        pool.query('INSERT INTO lobbyChat(user, message) VALUE(?,?)', ['user', msg], (err) => {
+            if(err){
+                console.log('Error inserting message into DB.');
+            }
+            else{
+                console.log('Message from user: ' + msg);
+                io.emit('chat message', msg);
+            }
+        });
+    });
+});
+
+/* Start server */
+server.listen(app.get('port'), function(){
+    console.log('Starting server on port ' + app.get('port'));
+});
+
+/* End connection with DB */
+pool.end
