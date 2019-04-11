ff6776fedaaa8377d5c0b8883e38f6dc68257ada
Balbalnom <diandianhanxing@gmail.com>
2019-04-10 23:53:45 -0700

    Update Login/Login Session/Passport

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var User = require("../controllers/user") //TODO User Controller
+var User = require("../controllers/user")
 
-router.post('/register',function(req,res){
-  const result = User.register(req);
-  console.log(result);
-  if(result.code == 200){
-    res.render('login',{message: result.message});
-  }else{
-    res.render('sign-up',{message: result.message});
-  }
+router.post('/register', User.register);
+router.get('/', function (req, res) {
+  res.render('/', { message: undefined });
 });
-router.get('/sign-up',function(req,res){
-  res.render('sign-up',{message:undefined});
+router.get('/', function (req, res) {
+  res.render('/', { message: undefined }); //Need to write login and reg page
 });
-router.get('/login',function(req,res){
-  res.render('login',{message: undefined});
-})
-
 module.exports = router;
--- server.js
+++ server.js
@@ -11,11 +11,45 @@ const io = socketIO(server);
 //User Router and model for passport
 const userRouter = require('./routes/users');
 const userModel = require('./models/index').User;
-//Session for Login using passport, express-session and bcrypt. bcrypt defined within lib bcrypt.js
+//Session for Login using passport, express-session and bcrypt.js
 var session = require("express-session");
 const {comparePassword} = require('./lib/bcrypt');
 var passport = require('passport')
   , LocalStrategy = require('passport-local').Strategy;
+
+  // passport user setup
+passport.use('local-login', new LocalStrategy(
+    function(username, password, action, done) {
+        userModel.findOne({ where: { userName: username }}).then(function(user, err) { //findOne() ajax call and nodejs called ajax as promise object
+            if(action==sign-up) //do sign-up buttom
+            {
+                if(err){
+                    return done(err);
+                }
+                if(user){
+                    return done(null, false, { message: 'username already exist' });
+                }else{
+                    return done(null,user);
+                }
+
+            }else{ //do login buttom
+                if (err) { return done(err); }
+                if (!user) {
+                    return done(null, false, { message: 'Incorrect username.' });
+                }
+                if (!comparePassword(password, user.password)) {
+                    return done(null, false, { message: 'Incorrect password.' });
+                }
+                return done(null, user);
+            }
+        });
+    }
+));
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.set('port', 8081);
 app.use(express.static(path.join(__dirname, 'public')));
@@ -24,53 +58,42 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Display index.html
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/index.html');
-});``
+});
+app.post('/', function(req, res, next) {
+    passport.authenticate('local-login', {
+      successRedirect : '/lobby', // redirect to the secure profile section
+      failureRedirect : '/', // redirect back to the signup page if there is an error
+      failureFlash : true // allow flash messages
+      },function(err, user, info) {
+          if (err) { return next(err); }
+          if (!user) { return res.redirect(info,'/'); }
+          req.logIn(user, function(err) {
+              if (err) { return next(err); }
+              return res.redirect('/lobby');
+          });
+    })(req, res, next);
+  });
+  //session
+passport.serializeUser(function(user, done) {
+    return done(null, user.id);
+});
+  
+passport.deserializeUser(function(id, done) {
+    userModel.findByPk(id).then(function(user, err) {
+        return done(err, user);
+    });
+});
 
-// Display login.html ok but better using routing method
+/*Display login.html ok but better using routing method  There is no login.html
 app.get('/login', function(req, res){
     res.sendFile(__dirname + '/public/login.html');
-});
+});*/
 // Display login.html
 app.get('/lobby', function(req, res){
     res.sendFile(__dirname + '/public/lobby.html');
 });
 
-/*login using passport in here */
-app.use(express.static("public"));
-app.use(session({ secret: "cats" }));
-app.use(passport.initialize());
-app.use(passport.session());
-app.post('/login',
-  passport.authenticate('local', { successRedirect: '/',
-                                   failureRedirect: '/login',
-                                   failureFlash: 'Invalid username or password.',
-                                   successFlash: 'Welcome!' }));
-//passport user setup
-passport.use(new LocalStrategy(
-    function(username, password, done) {
-      console.log(userModel);
-      userModel.findOne({ userName: username }, function (err, user) {
-        if (err) { return done(err); }
-        if (!user) {
-          return done(null, false, { message: 'Incorrect username.' });
-        }
-        if (!comparePassword(password, user.password)) {
-          return done(null, false, { message: 'Incorrect password.' });
-        }
-        return done(null, user);
-      });
-    }
-  ));
-  passport.serializeUser(function(user, done) {
-      done(null, user.id);
-  });
-    
-  passport.deserializeUser(function(id, done) {
-      userModel.findById(id, function(err, user) {
-        done(err, user);
-      });
-  });
-
+app.use('/',userRouter);
 
 /* Create database connection */
 var pool = mysql.createPool({
