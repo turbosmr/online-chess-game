aca7654c7a607e642fa567abb1b5ae7b49263ba5
STEVE RENTSCHLER <steverentschler@STEVEs-MacBook-Pro-2.local>
2019-05-12 19:09:33 -0700

    Merged from milestone eval
    Merged from milestone eval

--- models/user.js
+++ models/user.js
@@ -1,17 +1,34 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    userName: DataTypes.STRING,
-    password: DataTypes.STRING,
-    winCount: DataTypes.INTEGER,
-    loseCount: DataTypes.INTEGER,
-    drawCount: DataTypes.INTEGER,
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    winCount: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0
+    },
+    loseCount: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0
+    },
+    drawCount: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0
+    },
     rating: DataTypes.DOUBLE,
     isActive: DataTypes.BOOLEAN
   }, {});
   User.associate = function(models) {
     // associations can be defined here
-    User.belongsToMany(User, {as: 'Friends', through: models.Friendship});
   };
   return User;
 };
