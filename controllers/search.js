f5fcf4692dec22d6c73a41fa6d327e0a41c07a43
Johnson Wong <johnsonwong@Johnsons-MacBook-Pro.local>
2019-05-16 23:26:29 -0700

    fixed rendering of board and chesspieces per user's profile setting

--- controllers/search.js
+++
@@ -1,6 +0,0 @@
-const userModel = require('../models/index').User;
-
-const Op = require('sequelize').Op;
-exports.search = function (req, res) {
-    userModel.findAll({where: {userName:{[Op.like]: '%'+req.query.search+'%'}}});
-};
